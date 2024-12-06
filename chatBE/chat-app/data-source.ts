6beb2f17e2569a9abb96592307dd6d528d26d5d2
sihwan <stay136@gmail.com>
2024-12-06 18:09:03 +0900

    data-source

---
+++ chatBE/chat-app/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_POR || '3306'),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  entities: [
+    'dist/auth/entities/*.js',
+    'dist/chatting/entities/*.js',
+    'dist/users/entities/*.js',
+  ],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+  logging: true,
+});
