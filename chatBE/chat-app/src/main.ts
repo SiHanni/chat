607a6ffb44bb70900dbc3697852ec7e19b6c383c
sihwan <stay136@gmail.com>
2024-12-11 22:51:58 +0900

    for test github action

--- chatBE/chat-app/src/main.ts
+++ chatBE/chat-app/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   });
   app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(port ?? 3000);
-  logger.log(`Server is starting in port ${port}`);
+  logger.log(`Server is starting in port ${port}!`);
 }
 bootstrap();
