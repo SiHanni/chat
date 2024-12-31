23260f8e7b54c2bd51362831094eee3a3b344c96
sihwan <stay136@gmail.com>
2025-01-01 07:41:09 +0900

    new server url

--- chatFE/chatweb/src/common/serverConfig.ts
+++ chatFE/chatweb/src/common/serverConfig.ts
@@ -1,3 +1,4 @@
-const server_host = process.env.REACT_APP_SERVER_HOST || 'http://localhost';
+const server_host =
+  process.env.REACT_APP_SERVER_HOST || 'https://www.marutalk.com';
 const server_port = process.env.REACT_APP_SERVER_PORT || '3000';
 export const server_url = `${server_host}:${server_port}`;
